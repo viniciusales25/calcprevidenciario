319b8fa1736e156f2dd2d3e834b14c5814285901
viniciusales25 <viniciusales25@gmail.com>
2025-01-24 10:59:32 -0300

    Update alert.tsx

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,31 +1,44 @@
-import * as React from "react";
+import * as React from "react"
 
-const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
+interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  variant?: string;
+}
+
+interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  className?: string;
+}
+
+interface DescriptionProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(({ className, variant, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
     className={`relative w-full rounded-lg border p-4 [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7 ${className}`}
     {...props}
   />
-));
-Alert.displayName = "Alert";
+))
+Alert.displayName = "Alert"
 
-const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
+const AlertTitle = React.forwardRef<HTMLHeadingElement, TitleProps>(({ className, ...props }, ref) => (
   <h5
     ref={ref}
     className={`mb-1 font-medium leading-none tracking-tight ${className}`}
     {...props}
   />
-));
-AlertTitle.displayName = "AlertTitle";
+))
+AlertTitle.displayName = "AlertTitle"
 
-const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
+const AlertDescription = React.forwardRef<HTMLDivElement, DescriptionProps>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={`text-sm [&_p]:leading-relaxed ${className}`}
     {...props}
   />
-));
-AlertDescription.displayName = "AlertDescription";
+))
+AlertDescription.displayName = "AlertDescription"
 
-export { Alert, AlertTitle, AlertDescription };
+export { Alert, AlertTitle, AlertDescription }
